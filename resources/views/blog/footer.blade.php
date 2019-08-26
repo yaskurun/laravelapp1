2a8e925de5cb2ddf34d03f6fa2404ca88f58fd51
RUNWEB ID <yaskurun@gmail.com>
2019-08-26 09:51:32 +0700

    Test

--- resources/views/blog/footer.blade.php
+++ resources/views/blog/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="py-5 bg-info">
     <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Belajar Koding 2019</p>
+        <p class="m-0 text-center text-white">Copyright &copy; Belajar Koding | RUNWEB ID 2019 </p>
     </div>
 </footer>
 <!-- END : Footer -->
